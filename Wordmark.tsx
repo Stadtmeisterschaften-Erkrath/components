b2db38ccdc8455ad5e1761b6059f7c0a6fd973ed
toohard2explain <levihsmn@gmail.com>
2023-12-15 23:42:10 +0100

    introduce: Wordmark component

---
+++ Wordmark.tsx
@@ -0,0 +1,20 @@
+type Props = {
+    width?: number;
+    height?: number;
+    className?: string;
+}
+
+function getLogoUrl(): string {
+    return 'https://raw.githubusercontent.com/Stadtmeisterschaften-Erkrath/content/main/logo.png';
+}
+
+export default function Wordmark(props: Props) {
+    return (
+      <div className={props.className}>
+          <img
+              src={getLogoUrl()} height={props.height} width={props.width}
+              alt={'Stadtmeisterschaften Erkrath'}
+          />
+      </div>
+    );
+}
