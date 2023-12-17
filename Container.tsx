6adc7077321ced3dd8ff8546666f810b082bb9fb
toohard2explain <levihsmn@gmail.com>
2023-12-18 00:45:51 +0100

    feature: Add vertical padding to container component

--- Container.tsx
+++ Container.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function Container(props: Props) {
   return (
-    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-4">
       {props.children}
     </div>
   );
