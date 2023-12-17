079cdc7f8db406b3f77d4e8324cb379e744af3ee
toohard2explain <levihsmn@gmail.com>
2023-12-17 17:28:51 +0100

    introduce: Container component

---
+++ Container.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function Container(props: Props) {
+  return (
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      {props.children}
+    </div>
+  );
+}
