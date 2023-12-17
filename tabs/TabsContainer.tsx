2afec6fa754c3d8465037f28152953de14e6d269
toohard2explain <levihsmn@gmail.com>
2023-12-17 20:47:14 +0100

    introduce: TabsContainer component

---
+++ tabs/TabsContainer.tsx
@@ -0,0 +1,14 @@
+type Props = {
+    activeTab: string;
+    tabId: string;
+    className?: string;
+    children: React.ReactNode;
+};
+
+export default function TabsContainer(props: Props) {
+    return (
+        <div className={props.className} hidden={props.activeTab !== props.tabId}>
+            {props.children}
+        </div>
+    );
+}
